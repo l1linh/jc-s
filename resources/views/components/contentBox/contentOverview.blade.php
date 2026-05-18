1e196960ad3f56cd9ee515d6ba86cdafac74b6da
David Bui <dungpro96bn@gmail.com>
2026-05-18 11:54:19 +0700

    ovirew top

--- resources/views/components/contentBox/contentOverview.blade.php
+++ resources/views/components/contentBox/contentOverview.blade.php
@@ -1,7 +1,19 @@
 <div id="overview">
   <div class="contentInner">
-      <h2 class="contentHeading">
+      <h2 class="title-overview">
         地元で見つける！私らしい働き方
       </h2>
+      <dl class="dl-overview">
+        <dt class="dt-over">
+          ココジョブは、安心・信頼・実績のある<br>日本コンコードシステムが運営する<br class="sp-br">求人サイトです。
+        </dt>
+        <dd class="dd-over">
+          地元企業との長年のつながりを活かし、<br class="pc-br">
+        一人ひとり<br class="sp-br">の希望やライフスタイルに合ったお仕事をご提案。<br>
+        未経験からの挑戦も、経験を活かしたステップアッ<br class="sp-br">プも、<br class="pc-br">
+        あなたのペースで大丈夫。<br>
+        働くことを「続けられる安心」に変える<br class="sp-br">サポートがここにあります。<br>
+        </dd>
+      </dl>
   </div>
 </div>
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -57,6 +57,20 @@
     <script type="text/javascript" src="{{asset('js/jquery.flicksimple.js')}}"></script>
     <script type="text/javascript" src="{{asset('js/jquery.cbslideheader.min.js')}}"></script>
 
+
+    <script>
+        (function(d) {
+          var config = {
+            kitId: 'bwh6him',
+            scriptTimeout: 3000,
+            async: true
+          },
+          h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+        })(document);
+      </script>
+
+      
+
     @vite(['resources/js/app.js', $jsImportFilePath])
 
     <script>
