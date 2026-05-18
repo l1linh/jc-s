fd18c970fd827288224f2a0d8cf59cdd05871c60
David Bui <dungpro96bn@gmail.com>
2026-05-18 16:16:09 +0700

    top content

--- resources/views/components/contentBox/contentPickup.blade.php
+++ resources/views/components/contentBox/contentPickup.blade.php
@@ -1,8 +1,44 @@
 <div id="pickup">
   <div class="contentInner">
-      <h2 class="contentHeading">
+      <h2 class="contentHeading pic">
         <span class="en">Pick Up</span>
         <span class="ja">人気のお仕事検索</span>
       </h2>
+
+      <ul class="list-pic">
+        <li class="item-pic">
+            <h3 class="title-pic">\   未経験でも安心して働ける   /</h3>
+            <p class="box-img">
+              <picture>
+                  <source media="(max-width: 767px)" srcset="{{asset('images/contentBox/top/top_img_01_sp.png')}}">
+                  <source media="(min-width: 768px)" srcset="{{asset('images/contentBox/top/top_img_01_pc.png')}}">
+                  <img class="sizes" src="{{asset('images/contentBox/top/top_img_01_pc.png')}}" alt="">
+              </picture>
+            </p>
+            <button class="buton-pic">お仕事を探す</button>
+        </li>
+        <li class="item-pic">
+          <h3 class="title-pic">\   高時給でしっかり稼ぐ   /</h3>
+          <p class="box-img">
+            <picture>
+                <source media="(max-width: 767px)" srcset="{{asset('images/contentBox/top/top_img_02_sp.png')}}">
+                <source media="(min-width: 768px)" srcset="{{asset('images/contentBox/top/top_img_02_pc.png')}}">
+                <img class="sizes" src="{{asset('images/contentBox/top/top_img_02_pc.png')}}" alt="">
+            </picture>
+          </p>
+          <button class="buton-pic">お仕事を探す</button>
+      </li>
+      <li class="item-pic">
+        <h3 class="title-pic">\   ワークライフバランス重視   /</h3>
+        <p class="box-img">
+          <picture>
+              <source media="(max-width: 767px)" srcset="{{asset('images/contentBox/top/top_img_03_sp.png')}}">
+              <source media="(min-width: 768px)" srcset="{{asset('images/contentBox/top/top_img_03_pc.png')}}">
+              <img class="sizes" src="{{asset('images/contentBox/top/top_img_03_pc.png')}}" alt="">
+          </picture>
+        </p>
+        <button class="buton-pic">お仕事を探す</button>
+    </li>
+      </ul>
   </div>
 </div>
