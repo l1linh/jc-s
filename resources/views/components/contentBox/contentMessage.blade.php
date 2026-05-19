0cc7d33a7a33f251065e5b4b32fafc9f10c40957
David Bui <dungpro96bn@gmail.com>
2026-05-19 11:27:06 +0700

    top message

--- resources/views/components/contentBox/contentMessage.blade.php
+++ resources/views/components/contentBox/contentMessage.blade.php
@@ -1,8 +1,72 @@
-<div id="message">
-  <div class="contentInner">
-      <h2 class="contentHeading">
-        <span class="en">For message</span>
-        <span class="ja">現場で働く人の声をお届け</span>
-      </h2>
-  </div>
+
+
+<div class="block-bg">
+  <div id="message-job">
+    <div class="contentInner"> 
+       <ul class="list-mes">
+           <li class="item-mes">
+              <h2 class="title-mes">
+                <span class="en">For Message</span><br>
+                <span class="ja">現場で働く人の声をお届け</span>
+              </h2>
+              <div class="box-img">
+                <p class="text one">自分らしく働ける場所</p>
+                <p class="text two">見つけた！</p>
+                <picture>
+                    <source media="(max-width: 767px)" srcset="{{asset('images/contentBox/top/top_img_04_sp.png')}}">
+                    <source media="(min-width: 768px)" srcset="{{asset('images/contentBox/top/top_img_04_pc.png')}}">
+                    <img class="sizes" src="{{asset('images/contentBox/top/top_img_04_pc.png')}}" alt="">
+                </picture>
+            </div>
+              <div class="box-link">
+                <p class="text-box">実際に現場で働く人たちは、どんな想いでお仕事に向き合っているのか。派遣という働き方を選んだ理由は？ お仕事を通して感じたやりがいなど、求人情報だけでは伝わらな<br class="sp-br">い“リアルな声”をお届けします。<br class="pc-br">
+                  あなたが働く未来を想像するための、ヒントがきっと見つかります。
+                </p>
+                <a class="link-other" href="">詳しく見てみる
+                  <svg width="11" height="17" viewBox="0 0 11 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9.68369 7.5299C10.1211 7.92675 10.1211 8.61426 9.68369 9.01112L1.67193 16.2799C1.02932 16.8629 0 16.407 0 15.5393L0 1.00173C0 0.13406 1.02933 -0.3219 1.67193 0.261113L9.68369 7.5299Z" fill="#47B08B"/>
+                  </svg>
+                </a>
+     
+                  
+              </div>
+           </li>
+
+           <li class="item-mes">
+            <h2 class="title-mes">
+              <span class="en">For Job Seeker</span><br>
+              <span class="ja">ココジョブならきっと叶う！</span>
+            </h2>
+            <div class="box-img">
+              <p class="text three">あなたらしくを </p>
+              <p class="text four">見つけよう </p>
+              <picture>
+                  <source media="(max-width: 767px)" srcset="{{asset('images/contentBox/top/top_img_05_sp.png')}}">
+                  <source media="(min-width: 768px)" srcset="{{asset('images/contentBox/top/top_img_05_pc.png')}}">
+                  <img class="sizes" src="{{asset('images/contentBox/top/top_img_05_pc.png')}}" alt="">
+              </picture>
+          </div>
+            <div class="box-link">
+              <p class="text-box">地元で働きたい、生活リズムを大切にしたい、お仕事とプライベートを無理なく両立したい。そんな一人ひとりの想いに、コンコードは柔軟な働き方で応えています。<br>
+                勤務地や時間、お仕事の内容まで丁寧に向き合い、あなたらしく輝ける環境をご提案。安心して長く働けるよう、就業後もスタッフがそっと寄り添い続けます。
+              </p>
+              <a class="link-other" href="">詳しく見てみる
+                <svg width="11" height="17" viewBox="0 0 11 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M9.68369 7.5299C10.1211 7.92675 10.1211 8.61426 9.68369 9.01112L1.67193 16.2799C1.02932 16.8629 0 16.407 0 15.5393L0 1.00173C0 0.13406 1.02933 -0.3219 1.67193 0.261113L9.68369 7.5299Z" fill="#47B08B"/>
+                </svg>
+              </a>
+   
+                
+            </div>
+         </li>
+
+       </ul>
+     </div>
+
+   </div>
+  {{-- <x-contentBox.contentJobseeker /> --}}
+  <x-contentBox.contentAboutus />
+  <x-contentBox.contentWay />
+
+  
 </div>
